a5f658262f1d387b9389750091aac1ea9e869da3
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-08-19 19:28:54 +0900

    🐞 fix: tsc error

--- mobile-app/components/ui/icon/index.web.tsx
+++ mobile-app/components/ui/icon/index.web.tsx
@@ -68,7 +68,7 @@ type ParameterTypes = Omit<Parameters<typeof createIcon>[0], "Root">;
 const accessClassName = (style: any) => {
   const styleObject = Array.isArray(style) ? style[0] : style;
   const keys = Object.keys(styleObject);
-  return styleObject[keys[1]];
+  return keys[1] !== undefined ? styleObject[keys[1]] : undefined;
 };
 
 const createIconUI = ({ ...props }: ParameterTypes) => {
