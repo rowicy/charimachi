604427cb014559899c07624fb07ad1334d45c374
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-08-23 16:48:41 +0900

    ✨ feat: Go Server 接続に関するコメントを追加

--- mobile-app/env.ts
+++ mobile-app/env.ts
@@ -2,4 +2,6 @@ export const EXAMPLE_API_BASE_URL = "http://localhost:3001";
 // API base URL
 // Go Server: "http://localhost:8080/api/v1"
 // Prism Mock Server: "http://localhost:8080"
+// Go Server にスマホ実機で接続する場合は、PCのローカルIPアドレスを指定する
+// ifconfig > en0 > inet を確認
 export const API_BASE_URL = "http://localhost:8080/api/v1";
